6045eeaa02f60d2ce4a5fc56a1a0d2c91c7f70b8
Stan (Konstantin) <kostyacherepanov1@gmail.com>
2025-04-30 20:29:18 +0700

    fix

--- event/event.go
+++ event/event.go
@@ -1,17 +1,5 @@
 package event
 
 type TransactionFromSubscribedContract struct {
-	Network string `json:"string"`
-	Hash    string `json:"hash"`
-
-	From string `json:"from"`
-	To   string `json:"to,omitempty"`
-
-	GasPrice             string `json:"gas_price,omitempty"`
-	MaxFeePerGas         string `json:"max_fee_per_gas,omitempty"`
-	MaxPriorityFeePerGas string `json:"max_priority_fee_per_gas,omitempty"`
-
-	Value string `json:"value"`
-
-	Data []byte `json:"data"`
+	Transaction []byte
 }
